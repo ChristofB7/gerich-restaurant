83fd3c025d595e761e6a183b3b5e96a9ea68fb89
christof <christof@jazab.com>
2023-04-03 09:56:35 -0700

    Initial Project structure setup

---
+++ src/container/FindUs/FindUs.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import './FindUs.css'
+
+const FindUs = () => {
+    <div>
+        FindUs
+    </div>
+}
+
+export default FindUs
---
+++ src/container/Gallery/Gallery.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import './Gallery'
+
+const Gallery = () => (
+    <div>Gallery</div>
+)
+
+export default Gallery
