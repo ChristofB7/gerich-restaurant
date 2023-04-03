74d6cab7667450ca7caa278c81e54b817877c733
christof <christof@jazab.com>
2023-04-03 11:44:12 -0700

    implemented Navbar, Heading and SubHeading

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from 'react'
 
-import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
-import { Navbar } from './components';
-import './App.css';
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container'
+import { Navbar } from './components'
+import './App.css'
 
 const App = () => (
     <div>
@@ -19,4 +19,4 @@ const App = () => (
     </div>
 );
 
-export default App;
+export default App
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,8 +1,21 @@
 import React from "react"
 import './Header.css'
+import { SubHeading } from '../../components'
+import { images } from "../../constants"
 
 const Header = () => (
-    <div>Header</div>
+    <div className="app__header app__wrapper section__padding" id="home">
+        <div className="app__wrapper_info">
+            <SubHeading title='Chase the new flavor' />
+            <h1 className="app__header-h1">The Key to Fine Dining</h1>
+            <p className="p__opensans" style={{ margin: '2rem 0' }}>Pizza and neighborhood hang, Gericht serves both Detroit-style square and 12-inch round pies, served from a WoodStone hearth oven. Local farm goods from our friends throughout the PNW lead the seasonally changing menu. </p>
+            <button type="button" className="custom__button">Explore Menu</button>
+        </div>
+
+        <div className="app__wrapper_img">
+            <img src={images.welcome} alt="header" />
+        </div>
+    </div>
 )
 
 export default Header
