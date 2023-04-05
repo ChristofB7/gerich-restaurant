2e74dd22f1f61ef488813634e36184383578cef5
christof <christof@jazab.com>
2023-04-05 11:54:39 -0700

    added Chef, Video, and Laurels sections

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,8 +1,35 @@
 import React from "react"
+import { images } from '../../constants'
+import { SubHeading } from '../../components'
 import './Chef.css'
 
 const Chef = () => (
-    <div>Chef</div>
+    <div className="app__bg app__wrapper section__padding">
+        <div className="app__wrapper_img app__wrapper_img-reverse">
+            <img src={images.chef} alt="chef" />
+        </div>
+
+        <div className="app__wrapper_info">
+            <SubHeading title="Chef's word" />
+            <h1 className="headtext__cormorant"> What we believe in</h1>
+
+            <div className="app__chef-content">
+                <div className="app__chef-content_quote">
+                    <img src={images.quote} alt="quote" />
+                    <p className="p__opensans">
+                        Seasonal spritzes, a strong love for amaro and current
+                    </p>
+                </div>
+                <p className="p__opensans"> wine and beer crushes make for a refreshing cocktail.</p>
+            </div>
+
+            <div className="app__chef-sign">
+                <p>Kevin Luo</p>
+                <p className="p__opensans">Chef & Founder</p>
+                <img src={images.sign} alt="signature" />
+            </div>
+        </div>
+    </div>
 )
 
 export default Chef
--- src/container/FindUs/FindUs.jsx
+++ src/container/FindUs/FindUs.jsx
@@ -1,10 +1,8 @@
 import React from "react"
 import './FindUs.css'
 
-const FindUs = () => {
-    <div>
-        FindUs
-    </div>
-}
+const FindUs = () => (
+    <div>FindUs</div>
+)
 
 export default FindUs
